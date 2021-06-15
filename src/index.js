6a6d2d9b171790fb3fe18f554cb4c5c2ba896e3e
Fr3ud <oo.pavelko@gmail.com>
2021-06-15 21:17:21 +0300

    add SwapiService class

--- src/index.js
+++ src/index.js
@@ -1,47 +1,4 @@
-console.log('Hello World!')
-
-
-class SwapiService {
-    _apiBase = 'https://swapi.dev/api';
-
-    async getResource(url) {
-        return fetch(`${this._apiBase}${url}`).then((res) => {
-            if (!res.ok) throw new Error(`Could not fetch ${url}, received ${res.status}`);
-
-            return res.json()
-        });
-    }
-
-    async getAllPeople() {
-        const res = await this.getResource(`/people/`);
-
-        return res.results;
-    }
-
-    getPerson(id) {
-        return this.getResource(`/people/${id}`);
-    }
-
-    async getAllPlanets() {
-        const res = await this.getResource(`/planets/`);
-
-        return res.results;
-    }
-
-    getAllPlanet(id) {
-        return this.getResource(`/planet/${id}`);
-    }
-
-    async getAllStarships() {
-        const res = await this.getResource(`/starships/`);
-
-        return res.results;
-    }
-
-    getStarship(id) {
-        return this.getResource(`/starships/${id}`);
-    }
-}
+import SwapiService from './services/swapi-service';
 
 const swapi = new SwapiService();
 
