29ad251dc0527747cd551dabcadbe6b253d33b5b
Fr3ud <oo.pavelko@gmail.com>
2021-06-18 22:04:04 +0300

    add: index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import SwapiService from './services/swapi-service';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-const swapi = new SwapiService();
-
-swapi.getAllPeople().then((body) => console.log(body))
+ReactDOM.render(<App />, document.getElementById('root'));
