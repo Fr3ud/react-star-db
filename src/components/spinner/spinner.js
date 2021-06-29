1b56186ecda54782c665aef16278283ffab116bc
Fr3ud <oo.pavelko@gmail.com>
2021-06-29 22:30:31 +0300

    feat: add spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-bean-eater-ceo0tnvtwn8">
+            <div className="ldio-490z0bkn5x9">
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
