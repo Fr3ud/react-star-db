90c4f6b22f4a21240027546baf29498fadef6258
Fr3ud <oo.pavelko@gmail.com>
2021-07-08 22:01:54 +0300

    feat: add people list

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 
+import SwapiService from '../../services/swapi-service';
+
 import './item-list.css';
+import Spinner from "../spinner";
 
 export default class ItemList extends Component {
 
+  swapiService = new SwapiService();
+
+  state = {
+    peopleList: null,
+  };
+
+  componentDidMount() {
+    this.swapiService
+      .getAllPeople()
+      .then(peopleList => this.setState({ peopleList }));
+  }
+
+  renderItems = arr => {
+    return arr.map(person => {
+      return (
+        <li
+          className="list-group-item"
+          key={ person.id }
+          onClick={() => this.props.onItemSelected(person.id) }>
+          { person.name }
+        </li>
+      );
+    });
+  };
+
+
   render() {
+    const { peopleList } = this.state;
+
+    if (!peopleList) return <Spinner/>
+
+    const items = this.renderItems(peopleList);
+
     return (
       <ul className="item-list list-group">
-        <li className="list-group-item">
-          Luke Skywalker
-        </li>
-        <li className="list-group-item">
-          Darth Vader
-        </li>
-        <li className="list-group-item">
-          R2-D2
-        </li>
+        { items }
       </ul>
     );
   }
