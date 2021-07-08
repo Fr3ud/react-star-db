93f6dd7da2dd727ac6862eadbfbb545e2a8169a4
Fr3ud <oo.pavelko@gmail.com>
2021-07-08 22:04:10 +0300

    feat: add onPersonSelected method

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -7,22 +7,31 @@ import PersonDetails from '../person-details';
 
 import './app.css';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+export default class App extends Component {
+  state = {
+    showRandomPlanet: true,
+    selectedPerson: null,
+  };
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+  onPersonSelected = (id) => {
+    this.setState({ selectedPerson: id });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header/>
+        <RandomPlanet/>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={ this.onPersonSelected }/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={ this.state.selectedPerson }/>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default App;
+    );
+  };
+}
