0d7a5885a1f725239eef9e1dac6f8086e75100a8
Fr3ud <oo.pavelko@gmail.com>
2021-07-17 14:18:05 +0300

    feat: refactor component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,29 +2,83 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import ErrorIndicator from '../error-indicator';
+import PeoplePage from '../people-page';
+
+import ItemList from '../item-list/item-list';
+import PersonDetails from '../person-details/person-details';
+import SwapiService from '../../services/swapi-service';
 
 import './app.css';
-import PeoplePage from "../people-page";
 
 export default class App extends Component {
+  swapiService = new SwapiService();
+
   state = {
     showRandomPlanet: true,
-    selectedPerson: null,
+    hasError: false
   };
 
-  onPersonSelected = (id) => {
-    this.setState({ selectedPerson: id });
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
   }
 
   render() {
+
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
+
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet/> :
+      null;
+
     return (
-      <div>
-        <Header/>
-        <RandomPlanet/>
-        <PeoplePage/>
+      <div className="stardb-app">
+        <Header />
+
+        { planet }
+
+        <div className="row mb2 button-row">
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+          </button>
+        </div>
+
+        <PeoplePage />
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
       </div>
     );
-  };
+  }
 }
